1d3e65f5a6af3c6520f33303ae652ddb0dd95d1a
sasi182 <sandro.simmler@gmail.com>
2025-07-26 19:36:33 +0200

    add enemies and sound

---
+++ data/tilesets/TX Tileset Stone Ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="TX Tileset Stone Ground" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../graphics/TX Tileset Stone Ground.png" width="256" height="256"/>
+</tileset>
